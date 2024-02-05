f4931d3e0c6f7b34c1b6a9b0315479ce3a914edd
Md. Al amin <alamincsecu@gmail.com>
2024-02-05 23:03:42 +0600

    added default props and propsTypes

--- react-card/src/App.jsx
+++ react-card/src/App.jsx
@@ -9,9 +9,18 @@ function App() {
       <Button />
       <Card />
       <Card />
+      
+      {/* 
+      props = read only properties that are shared between components.
+      A parent component can send data to a child component
+      <Component key-value />
+      */}
+
       <Student name="Ariz" age={1.25} isStudent={false} />
       <Student name="Farhan" age={2} isStudent={false} />
       <Student name="Imtinan" age={5} isStudent={true} />
+      <Student />
+
     </>
   );
 }
--- react-card/src/StudentCard/Student.jsx
+++ react-card/src/StudentCard/Student.jsx
@@ -1,4 +1,7 @@
+
 import styles from './Student.module.css'
+import PropTypes from 'prop-types'
+
 function Student(props) {
     return (
         <div className={styles.student}>
@@ -8,5 +11,21 @@ function Student(props) {
         </div>
     );
 
+}
+// propTypes = a mechanism that ensures that 
+// the passed value is of the correct datatype. 
+// Such as age:PropTypes.number
+Student.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    isStudent: PropTypes.bool,
+}
+
+// defaultProps  = default values for props in case they are not passed from the parent Component. 
+// Such as name = 'guest'
+Student.defaultProps = {
+    name: "guest",
+    age: 0,
+    isStudent: false,
 }
 export default Student
